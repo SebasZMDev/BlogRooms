d1a96d55a8fa21b07606b9d24e02464afc1417d3
SebasZM <sebaszelada28@gmail.com>
2024-09-11 22:26:58 -0500

    color-votes

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -6,7 +6,7 @@ import { FaComment } from "react-icons/fa";
 import { IoMdShare } from "react-icons/io";
 import { useLocation, useNavigate } from 'react-router-dom';
 import ImgPreview from './ImgPreview';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IDContext, PostData, useUser } from '../App';
 import { getUserInfo } from '../hooks/getUserInfo';
 import { useSave } from '../hooks/useSave';
@@ -27,7 +27,7 @@ type Props = {
 
 
 
-const Post = ({ id, userID, eparent, content, media, score, negscore, repost, comments, fecha }: Props) => {
+const Post = ({ id, userID, eparent, content, media, repost, comments, fecha }: Props) => {
   const navigate = useNavigate();
   const location = useLocation();
   const isPostPreviewPage = location.pathname === '/pages/PostPreview';
@@ -37,6 +37,9 @@ const Post = ({ id, userID, eparent, content, media, score, negscore, repost, co
   const [imgSrc,setImgSrc] = useState<string>('')
   const {getUsername, getUserPFP, getUserThisPost} = getUserInfo();
   const postData = getUserThisPost(userID, id)
+  const [coloUp, setColorUp] = useState<boolean>(false)
+  const [colorDown, setColorDown] = useState<boolean>(false)
+  const [RePosted, setReposted] = useState<boolean>(false)
 
   const Preview = () => {
     if (!isPostPreviewPage) {
@@ -77,6 +80,7 @@ const Post = ({ id, userID, eparent, content, media, score, negscore, repost, co
        )
        postData.negscore = UpdatedList
       }
+      setColorDown(false)
     }
     if (alreadyLiked){
       if (user) {
@@ -87,6 +91,8 @@ const Post = ({ id, userID, eparent, content, media, score, negscore, repost, co
 
         const updatedUserLikes = user.userInfo.likes?.filter((like) => !(like.PUsername === user.username && like.PostID === id));
         user.userInfo.likes = updatedUserLikes?updatedUserLikes:null;
+
+        setColorUp(false)
 
         saveCurrentUser(user);
         const updatedList = usersList?.map((item) => item.id === user.id ? user : item) || [];
@@ -103,6 +109,8 @@ const Post = ({ id, userID, eparent, content, media, score, negscore, repost, co
 
         const updatedUserLikes = [...(user.userInfo.likes || []), newLike];
         user.userInfo.likes = updatedUserLikes;
+
+        setColorUp(true)
 
         saveCurrentUser(user)
         const updatedList = usersList?.map(item => item.id === user.id ? user : item) || [];
@@ -125,7 +133,8 @@ const Post = ({ id, userID, eparent, content, media, score, negscore, repost, co
       if (user){
         const UpdatedList = postData.score.filter(
           (element)=>(element.PUsername === user.username && element.PostID === id)
-        )
+        );
+        setColorUp(false)
         postData.score = UpdatedList
       }
     }
@@ -135,6 +144,8 @@ const Post = ({ id, userID, eparent, content, media, score, negscore, repost, co
           (element) => !(element.PUsername === user.username && element.PostID === id)
         );
         postData.negscore = UpdatedList;
+
+        setColorDown(false)
 
         saveCurrentUser(user);
         const updatedList = usersList?.map((item) => item.id === user.id ? user : item) || [];
@@ -159,6 +170,8 @@ const Post = ({ id, userID, eparent, content, media, score, negscore, repost, co
         const updatedUserLikes = user.userInfo.likes?.filter((like) => !(like.PUsername === user.username && like.PostID === id));
         user.userInfo.likes = updatedUserLikes?updatedUserLikes:null;
 
+        setColorDown(true)
+
         saveCurrentUser(user);
         const updatedList = usersList?.map((item) => item.id === user.id ? user : item) || [];
         saveUsersList(updatedList);
@@ -167,55 +180,68 @@ const Post = ({ id, userID, eparent, content, media, score, negscore, repost, co
     }
 }
 
-  const showScore = () =>{
-    console.log('Score',postData?.score)
-    console.log('NegScore',postData?.negscore)
+useEffect (()=>{
+  if (user && postData?.score && postData.negscore){
+    const Liked = postData.score.some(
+      (element)=>(element.PUsername === user.username && element.PostID === id)
+    );
+    const Disliked = postData.negscore.some(
+      (element)=>(element.PUsername === user.username && element.PostID === id)
+    );
+    if (Liked) {setColorUp(true)}
+    if (Disliked) {setColorDown(true)}
   }
+})
+
 
   return (
-    <div className='post-container' onClick={Preview}>
-      <div className='post-display-top'>
-        <div style={{display: 'grid', alignItems: 'center', justifyItems: 'center'}}>
-            <img className='post-pfp' src={getUserPFP(user?.id||'')}/>
-        </div>
-        <div className='post-username'>
-            <h4>{getUsername(user?.id||'')}</h4>
-            <h6>{fecha}</h6>
-        </div>
-      </div>
-      <div>
-     <div className='post-text'>{content}</div>
-        <div className='post-media'>
-        {media ? media.map((img, index) => (
-          <img
-            onClick={()=>OpenImgPreview(img)}
-            className='post-media-item'
-            style={{ width: media.length < 2 ? '200%' : '', height: media.length < 2 ? '200%' : '' }}
-            key={index}
-            src={img}
-            alt={`media-${index}`}
-          />
-        )) : ''}
-        </div>
-      </div>
-      <div className='post-display-bottom'>
-        <div className='post-score'>
-            <IoMdAdd onClick={VoteUpPost} className='post-buttons'/>
-            <h4 onClick={showScore} >{postData?.score ? postData?.score.length - (postData.negscore ? postData.negscore.length : 0) : 0}</h4>
-            <IoMdRemove onClick={VoteDownPost} className='post-buttons'/>
-        </div>
-        <div className='post-score'>
-          <IoMdRepeat className='post-buttons'/>
-          <h4>{repost}</h4>
-        </div>
-        <div className='post-score'>
-          <FaComment className='post-buttons'/>
-          <h4>{comments?comments.length:0}</h4>
-        </div>
-          <IoMdShare className='post-buttons'/>
-      </div>
-      {imgPrevDisplay && <ImgPreview imgSrc={imgSrc} closeImg={CloseImgPreview}/>}
+<div className='post-container' onClick={Preview}>
+  <div className='post-display-top'>
+    <div style={{ display: 'grid', alignItems: 'center', justifyItems: 'center' }}>
+      <img className='post-pfp' src={getUserPFP(user?.id || '')} />
     </div>
+    <div className='post-username'>
+      <h4>{getUsername(user?.id || '')}</h4>
+      <h6>{fecha}</h6>
+    </div>
+  </div>
+  <div>
+    <div className='post-text'>{content}</div>
+    <div className='post-media'>
+      {media ? media.map((img, index) => (
+        <img
+          onClick={(e) => {
+            e.stopPropagation();
+            OpenImgPreview(img);
+          }}
+          className='post-media-item'
+          style={{ width: media.length < 2 ? '200%' : '', height: media.length < 2 ? '200%' : '' }}
+          key={index}
+          src={img}
+          alt={`media-${index}`}
+        />
+      )) : ''}
+    </div>
+  </div>
+  <div className='post-display-bottom'>
+    <div className='post-score'>
+      <IoMdAdd style={coloUp ? { color: 'green' } : {}} onClick={(e) => { e.stopPropagation(); VoteUpPost();}} className='post-buttons' />
+      <h4 onClick={(e) => { e.stopPropagation(); }}>{postData?.score ? postData?.score.length - (postData.negscore ? postData.negscore.length : 0) : 0}</h4>
+      <IoMdRemove style={colorDown? {color: 'red'}:{}} onClick={(e) => { e.stopPropagation(); VoteDownPost();}} className='post-buttons' />
+    </div>
+    <div className='post-score'>
+      <IoMdRepeat className='post-buttons' />
+      <h4>{repost}</h4>
+    </div>
+    <div className='post-score'>
+      <FaComment className='post-buttons' />
+      <h4>{comments ? comments.length : 0}</h4>
+    </div>
+    <IoMdShare className='post-buttons' />
+  </div>
+  {imgPrevDisplay && <ImgPreview imgSrc={imgSrc} closeImg={CloseImgPreview} />}
+</div>
+
   );
 };
 
